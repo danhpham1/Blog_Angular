722af1a0e4ce92e7510d2e837c114fda29012b1c
cn16a1 <danhpham511998@gmail.com>
2020-08-26 23:50:51 +0700

    fix routerLink and menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from "../service/category.service";
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
@@ -7,16 +9,23 @@ import { CategoryService } from "../service/category.service";
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  currentUrl: string = ''
   categoryList: Array<any> = []
 
-  constructor(private categoryService: CategoryService) { }
+  constructor(private categoryService: CategoryService, private route: Router) { }
 
   ngOnInit(): void {
     this.categoryService.getCategory()
       .subscribe(categorys => {
         // console.log(categorys["data"]);
         this.categoryList = categorys["data"];
+        this.route.events.subscribe((res) => {
+          // console.log(this.route.url);
+          let urlStem = this.route.url.split('/');
+          this.currentUrl = urlStem[urlStem.length - 1];
+          // console.log(this.currentUrl == '');
+          // console.log(this.currentUrl);
+        })
       })
   }
 
