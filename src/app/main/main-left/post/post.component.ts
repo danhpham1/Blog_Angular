a2632029381be518cd07d7366056a36984a0b9b9
cn16a1 <danhpham511998@gmail.com>
2020-08-27 11:19:57 +0700

    fix routerLink post

--- src/app/main/main-left/post/post.component.ts
+++ src/app/main/main-left/post/post.component.ts
@@ -15,36 +15,35 @@ export class PostComponent implements OnInit {
   isLoad: boolean = false;
   parser = new DOMParser();
 
-  @Output() nameQuery = new EventEmitter<string>();
   constructor(private postService: PostService, private ngxSpinService: NgxSpinnerService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    // console.log(this.route.snapshot.routeConfig["path"]);
-
-    this.nameQuery.emit(this.route.snapshot.routeConfig["path"]);
-    this.postService.getPost(this.route.snapshot.params["id"])
-      .subscribe(post => {
-        this.ngxSpinService.show();
-        this.isLoad = true;
-        if (post["result"] == 1) {
-          this.post = post["data"];
-          //change url img
-          let changeSrcImg = this.parser.parseFromString(this.post["content"], 'text/html');
-          changeSrcImg.body.querySelectorAll("img").forEach(el => {
-            let valSrc: any = el.src;
-            valSrc = valSrc.split("/");
-            el.src = "";
-            el.src = "https://immense-eyrie-97426.herokuapp.com/img/post-img/" + valSrc[valSrc.length - 1];
-          });
-          this.contentPost = changeSrcImg.body.innerHTML;
-          // console.log(this.contentPost);
-          this.postService.getAllPost(this.post["nameTitle"]).subscribe(posts => {
-            this.relatePosts = [...posts["data"]].filter(el => el._id != this.post._id);
-            this.isLoad = false;
-            this.ngxSpinService.hide();
-          })
-        }
-      })
+    this.route.params.subscribe(param => {
+      this.postService.getPost(param["id"])
+        .subscribe(post => {
+          this.ngxSpinService.show();
+          this.isLoad = true;
+          if (post["result"] == 1) {
+            this.post = post["data"];
+            //change url img
+            let changeSrcImg = this.parser.parseFromString(this.post["content"], 'text/html');
+            changeSrcImg.body.querySelectorAll("img").forEach(el => {
+              let valSrc: any = el.src;
+              valSrc = valSrc.split("/");
+              el.src = "";
+              el.src = "https://immense-eyrie-97426.herokuapp.com/img/post-img/" + valSrc[valSrc.length - 1];
+            });
+            this.contentPost = changeSrcImg.body.innerHTML;
+            // console.log(this.contentPost);
+            this.postService.getAllPost(this.post["nameTitle"]).subscribe(posts => {
+              this.relatePosts = [...posts["data"]].filter(el => el._id != this.post._id);
+              this.isLoad = false;
+              window.scroll(0, 0);
+              this.ngxSpinService.hide();
+            })
+          }
+        })
+    })
   }
 
 }
